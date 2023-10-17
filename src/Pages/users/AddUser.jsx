e4e783c35bedaf3a234d992e2d7ef4df719ccf95
JANGAP411 <angapj@gmail.com>
2023-10-17 16:58:26 +1000

    fixed AddUser.jsx added input labels for the form inputs

--- src/Pages/users/AddUser.jsx
+++ src/Pages/users/AddUser.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import Page from "../../components/Page";
-import { Grid, TextField } from "@mui/material";
+import { Grid, InputLabel, TextField } from "@mui/material";
 
 const AddUser = () => {
   return (
     <Page>
       <Grid item container spacing={2}>
         <Grid item xs={12} sm={6}>
-          full name
+          <InputLabel id="full-name">Full Name</InputLabel>
           <TextField variant="standard" name="user-name-field" />
         </Grid>
         <Grid item xs={12} sm={6}>
-          username
-          <TextField variant="standard" name="user-name-field" />
+          <InputLabel id="username">Username</InputLabel>
+          <TextField variant="standard" name="user-name-field" type="text" />
         </Grid>
         <Grid item xs={12} sm={6}>
-          email
-          <TextField variant="standard" name="user-name-field" />
+          <InputLabel id="email">Email</InputLabel>
+          <TextField variant="standard" name="email-field" type="email" />
         </Grid>
         <Grid item xs={12} sm={6}>
-          password
-          <TextField variant="standard" name="user-name-field" />
+          <InputLabel id="password">Password</InputLabel>
+          <TextField variant="standard" name="password-field" type="password" />
         </Grid>
       </Grid>
     </Page>
